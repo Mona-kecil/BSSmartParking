a46daade5a00dc1559cbd8967aa39efc994cae92
Mona-kecil <flowyourminds@gmail.com>
2024-12-22 19:47:00 +0700

    Add README; minor fix

---
+++ README.md
@@ -0,0 +1,21 @@
+# Note
+## uv
+`uv` is great, please use it.
+
+## pnpm
+`pnpm` is great, please use it.
+
+## Dependency:
+- uv (this project use several python version, using uv will greatly help you)
+- pnpm (npm but better)
+
+## How to setup
+### Setting up backend
+1. cd into bs-backend
+2. `uv venv`
+3. `uv pip install -r requirements.txt`
+
+### Setting up ml-2
+1. cd into bs-ml-2
+2. `uv venv`
+3. `uv pip install -r requirements.txt`
--- start_system.bat
+++ start_system.bat
@@ -3,7 +3,7 @@
 start cmd /K "cd /d %cd%\bs-backend && .\.venv\Scripts\activate && python app/models/database.py && uvicorn app.main:app --reload"
 
 :: Start bs-frontend in a new terminal window
-start cmd /K "cd /d %cd%\bs-frontend && pnpm run dev"
+start cmd /K "cd /d %cd%\bs-frontend && pnpm install && pnpm run dev"
 
 :: Start bs-ml-2 in a new terminal window
-start cmd /K "cd /d %cd%\bs-ml-2 && .\.venv\Scripts\activate && python run src/main.py"
+start cmd /K "cd /d %cd%\bs-ml-2 && .\.venv\Scripts\activate && python src/main.py"
